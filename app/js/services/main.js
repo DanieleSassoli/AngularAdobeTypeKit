0fd57188541a164e338c8a5f7b63b8c2d1fb905a
DanieleSassoli <danielesassoli@gmail.com>
2015-11-12 13:00:26 +0100

    Added post requests.

--- app/js/services/main.js
+++ app/js/services/main.js
@@ -2,47 +2,64 @@
 
 angular.module('ngTypeKit').factory('mainService', ['$http', function ($http) {
     var baseUrl = 'https://typekit.com/api/v1/json/';
-
-    function executeRequest(endPoint, cb) {
-        var params = {
-            headers: {
-                'X-Typekit-Token': '15a9a8fd8623e13b0e2119d88b179b20232e483e'
-            }
-        };
+    var params = {headers: {'X-Typekit-Token': '15a9a8fd8623e13b0e2119d88b179b20232e483e'}};
+    function executeGetRequest(endPoint, cb) {
         $http.jsonp(baseUrl + endPoint + '?callback=JSON_CALLBACK', params).success(function (data) {
             cb(null, data);
         }).error(function (err) {
             cb(err);
         });
     }
-
+    function executePostRequest(endPoint, data, cb){
+        $http.post(baseUrl + endPoint + '?callback=JSON_CALLBACK', data, params).success(function (data) {
+            cb(null, data);
+        }).error(function (err) {
+            cb(err);
+        });
+    }
+    function executeDeleteRquest(endPoint, cb){
+        $http.delete(baseUrl + endPoint + '?callback=JSON_CALLBACK', params).success(function (data) {
+            cb(null, data);
+        }).error(function (err) {
+            cb(err);
+        });
+    }
     return {
         getFamily: function (familyId, cb){
-            executeRequest('families/' + familyId, cb);
+            executeGetRequest('families/' + familyId, cb);
         },
         getFamilyVariation: function (familyId, variationId, cb){
-            executeRequest('families/' + familyId + '/' + variationId, cb);
+            executeGetRequest('families/' + familyId + '/' + variationId, cb);
         },
         getKits: function (cb){
-            executeRequest('kits', cb);
+            executeGetRequest('kits', cb);
         },
         getKit: function (kitId, cb){
-            executeRequest('kits/' + kitId, cb);
+            executeGetRequest('kits/' + kitId, cb);
+        },
+        createKit: function(options, cb){
+            executePostRequest('kits', options, cb);
+        },
+        updateKit: function(kitId, options, cb){
+            executePostRequest('kits/' + kitId, options, cb);
+        },
+        updateFontFamily: function(kitId, familyId, options, cb){
+            executePostRequest('kits/' + kitId + 'families/' + familyId, options, cb);
         },
         getKitFamily: function (kitId, familyId, cb){
-            executeRequest('kits/' + kitId + 'families/' + familyId, cb);
+            executeGetRequest('kits/' + kitId + 'families/' + familyId, cb);
         },
         publishKit: function(kitId, cb){
-            executeRequest(kitId + 'publish', cb);
+            executePostRequest(kitId + 'publish', cb);
         },
         publishedKit: function(kitId, cb){
-            executeRequest(kitId + 'published', cb);
+            executeGetRequest(kitId + 'published', cb);
         },
         fontLibraries: function(cb){
-            executeRequest('libraries', cb);
+            executeGetRequest('libraries', cb);
         },
         fontLibrary: function(libraryId, cb){
-            executeRequest('libraries/' + libraryId, cb);
+            executeGetRequest('libraries/' + libraryId, cb);
         }
     }
 }]);
