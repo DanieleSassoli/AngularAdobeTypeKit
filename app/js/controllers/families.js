c737dab09d9d3fb666bdf59e0bd9648a56831676
DanieleSassoli <danielesassoli@gmail.com>
2015-11-13 16:16:59 +0100

    Added methods for families.

--- app/js/controllers/families.js
+++ app/js/controllers/families.js
@@ -1,12 +1,23 @@
 'use strict';
 
-angular.module('ngTypeKit').controller('families', ['$scope', 'ngTypeKitService', function($scope, ngTypeKitService){
+angular.module('ngTypeKit').controller('families', ['$scope', 'ngTypeKitService', function ($scope, ngTypeKitService) {
     $scope.family = {};
-    function manageError(err){
+    function manageError(err) {
         $scope.errorMsg = err;
         $scope.showAlert = true;
     }
-    $scope.getFamily = function(){};
-    $scope.getFamilyVariation = function(){};
+
+    $scope.getFamily = function () {
+        ngTypeKitService.getFamily($scope.family.familyId, function (err, data) {
+            if (err) manageError(err);
+            else $scope.family = data;
+        });
+    };
+    $scope.getFamilyVariation = function () {
+        ngTypeKitService.getFamilyVariation($scope.family.familyId, $scope.family.variationId, function (err, data) {
+            if (err) manageError(err);
+            else $scope.variation = data;
+        });
+    };
 }]);
 
